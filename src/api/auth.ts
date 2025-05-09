aed07a37426327470b568ce20835e557ec6dc2da
Desisacc <saccescuela@gmail.com>
2025-05-09 14:19:23 -0600

    Session cookie change
    The session cookie will always be sent as the "Authorization" header in requests to the backend.

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,7 @@
 import { SigninDataType, SigninResponseType, LoginDataType, LoginResponseType, ValidateResponseType } from "@schemas/auth";
-import Cookies from "js-cookie";
 import axios from "./axios";
 
 export const signinRequest = (data: SigninDataType) => axios.post<SigninResponseType>("/signin", data);
 export const loginRequest = (data: LoginDataType) => axios.post<LoginResponseType>("/login", data);
 
-export const validateSessionRequest = () => axios.get<ValidateResponseType>("/validate", {
-    headers: {
-        "Authorization": Cookies.get(import.meta.env.VITE_SESSION_COOKIE!)
-    },
-});
+export const validateSessionRequest = () => axios.get<ValidateResponseType>("/validate");
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true,
+    headers: {
+        "Authorization": Cookies.get(import.meta.env.VITE_SESSION_COOKIE!)
+    },
 });
 
 export default instance;
